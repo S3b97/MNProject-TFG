5d493ffebbce8a3d0cf1e6ba1bac933c1101a454
S3b97 <sebas16497@outlook.com>
2023-02-20 18:35:27 +0100

    adding feature register and minor changes in login

--- app/src/main/java/com/example/mnproject/view/MainActivity.java
+++ app/src/main/java/com/example/mnproject/view/MainActivity.java
@@ -2,15 +2,43 @@ package com.example.mnproject.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.mnproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    textView=(TextView)findViewById(R.id.register);
+    textView.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
+            startActivity(intent);
+        }
+    });
+
+
+    button=(Button)findViewById(R.id.join_us);
+    button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+            startActivity(intent);
+        }
+    });
+
+
     }
 }
